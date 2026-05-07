35e1d3d05d4344a27419d927f668ebaf96b4a232
Hector <hectormartinezcabedo@gmail.com>
2026-05-07 19:22:28 +0200

    Println de ejercicio7

--- Prueba/src/App.java
+++ Prueba/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Mario Hernandez Alegre!");
         System.out.println("Hector Martinez Cabedo");
+        System.out.println("ejercicio 7");
     }
 }
