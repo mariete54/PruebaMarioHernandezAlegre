d61e2b67137aa329676c7405b8b5d92309e9a400
Hector <hectormartinezcabedo@gmail.com>
2026-05-07 19:14:02 +0200

    Actualizacion codigo

--- Prueba/src/App.java
+++ Prueba/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Mario Hernandez Alegre!");
+        System.out.println("Hector Martinez Cabedo");
     }
 }
